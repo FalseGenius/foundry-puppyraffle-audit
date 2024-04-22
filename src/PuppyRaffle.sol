16999cfa02c8874e0348beb7612d5ed13ea84aa6
FalseGenius <billionairejackal@gmail.com>
2024-04-22 19:55:35 +0500

    Potential vulnerabilites/bugs identified

--- src/PuppyRaffle.sol
+++ src/PuppyRaffle.sol
@@ -162,6 +162,7 @@ contract PuppyRaffle is ERC721, Ownable {
         uint256 tokenId = totalSupply();
 
         // We use a different RNG calculate from the winnerIndex to determine rarity
+        // @audit insecure randomness vulnerability here as well. 
         uint256 rarity = uint256(keccak256(abi.encodePacked(msg.sender, block.difficulty))) % 100;
         if (rarity <= COMMON_RARITY) {
             tokenIdToRarity[tokenId] = COMMON_RARITY;
@@ -174,6 +175,8 @@ contract PuppyRaffle is ERC721, Ownable {
         delete players;
         raffleStartTime = block.timestamp;
         previousWinner = winner;
+
+        // @audit Winner won't get their money if their fallback is messed up. Is it bad....
         (bool success,) = winner.call{value: prizePool}("");
         require(success, "PuppyRaffle: Failed to send prize pool to winner");
         _safeMint(winner, tokenId);
